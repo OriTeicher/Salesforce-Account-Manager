797d94bf5d313bd501a21ab68909f1406dc48730
OriTeicher <oriteicherudemy@gmail.com>
2024-06-26 19:15:51 +0300

    prettier config

--- force-app/main/default/lwc/caseActivityLog/caseActivityLog.js
+++ force-app/main/default/lwc/caseActivityLog/caseActivityLog.js
@@ -1,8 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement } from "lwc"
 
 export default class CaseActivityLog extends LightningElement {
-connectedCallback(){
-    console.log('check')
-}
-
+  connectedCallback() {
+    console.log("test")
+  }
 }
